a244e37c16f21d7357806f183e0169f364a6fc58
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-02-01 09:55:13 -0600

    Updates

---
+++ precache-manifest.c227dbaa8dfc02d77f5aa8e75edfedeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a75de07983af45a62362fa4c139da6",
+    "url": "/pixabay-hooks-practices/index.html"
+  },
+  {
+    "revision": "ecc7100f40889921e7af",
+    "url": "/pixabay-hooks-practices/static/js/2.095435dc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pixabay-hooks-practices/static/js/2.095435dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303c4b699283d3344da6",
+    "url": "/pixabay-hooks-practices/static/js/main.7ca083ae.chunk.js"
+  },
+  {
+    "revision": "acc1ebb361f47eb86c14",
+    "url": "/pixabay-hooks-practices/static/js/runtime-main.024c0dbd.js"
+  }
+]);
